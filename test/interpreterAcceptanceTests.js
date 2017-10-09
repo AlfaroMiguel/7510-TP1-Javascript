bc870f2b7c9badd0f2929b95266f2d294e4d25a1
AlfaroMiguel <miguel.alfaro.a95@gmail.com>
2017-10-09 03:07:17 -0300

    More databases added

--- test/interpreterAcceptanceTests.js
+++ test/interpreterAcceptanceTests.js
@@ -5,7 +5,7 @@ var assert = require('assert');
 var Interpreter = require('../src/interpreter');
 
 
-describe("Interpreter", function () {
+describe("Interpreter - parent db", function () {
 
     var db = [
         "varon(juan).",
@@ -124,30 +124,36 @@ describe("Interpreter", function () {
     describe('Interpreter Errors', function () {
 
         it('hijo(pepe, jua should throw a InvalidQueryError', function () {
+            var errorName = "";
             try{
                 interpreter.checkQuery('hijo(pepe, jua');
             }
             catch(e){
-                assert(e.name == "Invalid Query");
+                errorName = e.name;
             };
+            assert(errorName == "Invalid Query");
         });
 
         it('(cecilia) should throw a InvalidQueryError', function () {
+            var errorName = "";
             try{
                 interpreter.checkQuery('(cecilia)');
             }
             catch(e){
-                assert(e.name == "Invalid Query");
+                errorName = e.name;
             };
+            assert(errorName == "Invalid Query");
         });
 
         it('padre should throw a InvalidQueryError', function () {
+            var errorName = "";
             try{
                 interpreter.checkQuery('padre');
             }
             catch(e){
-                assert(e.name == "Invalid Query");
+                errorName = e.name;
             };
+            assert(errorName == "Invalid Query");
         });
 
     });
@@ -155,4 +161,256 @@ describe("Interpreter", function () {
 
 });
 
+
+describe("Interpreter - number db", function () {
+
+    var db = [
+        "add(zero, zero, zero).",
+        "add(zero, one, one).",
+        "add(zero, two, two).",
+        "add(one, zero, one).",
+        "add(one, one, two).",
+        "add(one, two, zero).",
+        "add(two, zero, two).",
+        "add(two, one, zero).",
+        "add(two, two, one).",
+        "subtract(X, Y, Z) :- add(Y, Z, X)."
+    ];
+
+    var interpreter = null;
+
+    before(function () {
+        // runs before all tests in this block
+    });
+
+    after(function () {
+        // runs after all tests in this block
+    });
+
+    beforeEach(function () {
+        // runs before each test in this block
+        interpreter = new Interpreter();
+        interpreter.parseDB(db);
+    });
+
+    afterEach(function () {
+        // runs after each test in this block
+    });
+
+
+    describe('Interpreter Facts', function () {
+
+        it('add(zero, zero, zero) should be true', function () {
+            assert(interpreter.checkQuery('add(zero, zero, zero)'));
+        });
+
+        it('add(zero, zero, one) should be false', function () {
+            assert(interpreter.checkQuery('add(zero, zero, one)') == false);
+        });
+
+        it('add(one, one, two) should be true', function () {
+            assert(interpreter.checkQuery('add(one, one, two)'));
+        });
+
+        it('add(zero, two, two) should be true', function () {
+            assert(interpreter.checkQuery('add(zero, two, two)'));
+        });
+
+        it('add(zero, two, zero) should be false', function () {
+            assert(interpreter.checkQuery('add(zero, two, zero)') == false);
+        });
+
+        it('add(two, one, one) should be false', function () {
+            assert(interpreter.checkQuery('add(two, one, one)') == false);
+        });
+
+        it('add(zero, one, two) should be true', function () {
+            assert(interpreter.checkQuery('add(zero, one, two)') == false);
+        });
+
+    });
+
+    describe('Interpreter Rules', function () {
+
+        it('subtract(one, one, two) should be false', function () {
+            assert(interpreter.checkQuery('subtract(one, one, two)') == false);
+        });
+
+        it('subtract(two, one, one) should be true', function () {
+            assert(interpreter.checkQuery('subtract(two, one, one)'));
+        });
+
+        it('subtract(one, one, zero) should be true', function () {
+            assert(interpreter.checkQuery('subtract(one, one, zero)'));
+        });
+
+        it('subtract(one, one, one) should be false', function () {
+            assert(interpreter.checkQuery('subtract(one, one, one)') == false);
+        });
+
+        it('subtract(two, one, one) should be false', function () {
+            assert(interpreter.checkQuery('subtract(two, one, one)'));
+        });
+
+    });
+
+
+    describe("Interpreter - friends db", function () {
+
+        var db = [
+            "friend(pepe, juan).",
+            "friend(juan, franco).",
+            "friend(franco, pedro).",
+            "friend(miguel, tomas).",
+            "friend(tomas, pablo).",
+            "friend(gabriel, miguel).",
+            "friend(marcos, martin).",
+            "enemy(miguel, martin).",
+            "enemy(gabriel, marcos).",
+            "enemy(miguel, pepe).",
+            "enemy(tomas, juan).",
+            "group(X, Y, Z) :- friend(X, Y), friend(Y, Z).",
+            "fight(X, Y, Z, W) :- friend(X, Y), friend(Z, W), enemy(X, Z), enemy(Y, W)."
+        ];
+
+        var interpreter = null;
+
+        before(function () {
+            // runs before all tests in this block
+        });
+
+        after(function () {
+            // runs after all tests in this block
+        });
+
+        beforeEach(function () {
+            // runs before each test in this block
+            interpreter = new Interpreter();
+            interpreter.parseDB(db);
+        });
+
+        afterEach(function () {
+            // runs after each test in this block
+        });
+
+
+        describe('Interpreter Facts', function () {
+
+            it('friend(pepe, juan) should be true', function () {
+                assert(interpreter.checkQuery('friend(pepe, juan)'));
+            });
+
+            it('friend(franco, pedro) should be true', function () {
+                assert(interpreter.checkQuery('friend(franco, pedro)'));
+            });
+
+            it('friend(tomas, pablo) should be true', function () {
+                assert(interpreter.checkQuery('friend(tomas, pablo)'));
+            });
+
+            it('friend(pepe, miguel) should be false', function () {
+                assert(interpreter.checkQuery('friend(pepe, miguel)') == false);
+            });
+
+            it('friend(tomas, mariano) should be false', function () {
+                assert(interpreter.checkQuery('friend(tomas, mariano)') == false);
+            });
+
+            it('friend(micaela, juan) should be false', function () {
+                assert(interpreter.checkQuery('friend(micaela, juan)') == false);
+            });
+
+            it('enemy(miguel, pepe) should be true', function () {
+                assert(interpreter.checkQuery('enemy(miguel, pepe)'));
+            });
+
+            it('enemy(tomas, juan) should be true', function () {
+                assert(interpreter.checkQuery('enemy(tomas, juan)'));
+            });
+
+            it('enemy(pedro, juan) should be false', function () {
+                assert(interpreter.checkQuery('friend(micaela, juan)') == false);
+            });
+
+            it('enemy(pepe, juan) should be false', function () {
+                assert(interpreter.checkQuery('friend(micaela, juan)') == false);
+            });
+
+        });
+
+        describe('Interpreter Rules', function () {
+
+            it('group(pepe, juan, franco) should be true', function () {
+                assert(interpreter.checkQuery('group(pepe, juan, franco)'));
+            });
+
+            it('group(miguel, tomas, pablo) should be true', function () {
+                assert(interpreter.checkQuery('group(miguel, tomas, pablo)'));
+            });
+
+            it('group(pepe, juan, tomas) should be false', function () {
+                assert(interpreter.checkQuery('group(pepe, juan, tomas)') == false);
+            });
+
+            it('group(tomas, pablo, pepe) should be false', function () {
+                assert(interpreter.checkQuery('group(tomas, pablo, pepe)') == false);
+            });
+
+            it('fight(gabriel, miguel, marcos, martin) should be true', function () {
+                assert(interpreter.checkQuery('fight(gabriel, miguel, marcos, martin)'));
+            });
+
+            it('fight(miguel, tomas, pepe, juan) should be true', function () {
+                assert(interpreter.checkQuery('fight(miguel, tomas, pepe, juan)'));
+            });
+
+        });
+
+    });
+
+    describe("Interpreter - Incomplete db", function () {
+
+        var db = [
+            "varon(juan).",
+            "varon"
+        ];
+
+        var interpreter = null;
+
+        before(function () {
+            // runs before all tests in this block
+        });
+
+        after(function () {
+            // runs after all tests in this block
+        });
+
+        beforeEach(function () {
+            // runs before each test in this block
+            interpreter = new Interpreter();
+        });
+
+        afterEach(function () {
+            // runs after each test in this block
+        });
+
+
+        describe('Incomplete database', function () {
+
+            it('Incomplete database should throw InvalidDatabaseError', function () {
+                var errorName = "";
+                try{
+                    interpreter.parseDB(db);
+                }
+                catch(e){
+                    errorName = e.name;
+                }
+                assert(errorName == "Invalid Database");
+            });
+
+        });
+
+
+        });
+});
 
